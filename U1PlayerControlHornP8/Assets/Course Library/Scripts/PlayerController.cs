d0dd77eb876a06f362868aa7c0d7085f003950a4
Rafaelakamexican <739415@student.dusd.net>
2025-03-10 15:28:31 -0700

    3/10/25
    Finished up the camera scripts and started the code for car movement?

--- U1PlayerControlHornP8/Assets/Course Library/Scripts/PlayerController.cs
+++ U1PlayerControlHornP8/Assets/Course Library/Scripts/PlayerController.cs
@@ -13,9 +13,13 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     public float speed = 20;
 
+    public float turnSpeed;
+    public float horizontalInput;
     void Update()
     {
+        horizontalInput = Input.GetAxis("Horizontal");
         // Move the vehicle forward
         transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
