215065989f0701a2b837d6f278b1cbd9b81bb8bd
Rafaelakamexican <739415@student.dusd.net>
2025-03-12 14:52:04 -0700

    3/12/25
    Finished all of the the car obstacles part

--- U1PlayerControlHornP8/Assets/Course Library/Scripts/PlayerController.cs
+++ U1PlayerControlHornP8/Assets/Course Library/Scripts/PlayerController.cs
@@ -11,15 +11,18 @@ public class PlayerController : MonoBehaviour
     }
 
     // Update is called once per frame
-    public float speed = 20;
-
-    public float turnSpeed;
-    public float horizontalInput;
+    private float speed = 20.0f;
+    private float turnSpeed = 45.0f;
+    private float horizontalInput;
+    private float forwardInput;
     void Update()
     {
         horizontalInput = Input.GetAxis("Horizontal");
         // Move the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
-        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
+        forwardInput = Input.GetAxis("Vertical");
+        // / Move the van forward based on vertical time
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        // rotates the van based on horizontal input
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
     }
 }
